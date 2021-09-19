a15e2d944e3405287500b68f113ad74d1041382b
ar4ben <arthur.rodzkin@gmail.com>
2021-09-20 02:17:33 +0300

    refactor main scripts

--- src/background.js
+++ src/background.js
@@ -3,47 +3,40 @@
 
 let goals;
 let infotab;
-let maintabId;
 let debugTabId;
 const requests = {};
 const matches = {};
 
-function startFromWhiteList() {
+const startFromWhiteList = () => {
   chrome.storage.local.clear();
-}
+};
 
-function onAttach() {
-  if (chrome.runtime.lastError) {
-    alert(chrome.runtime.lastError.message);
+const onAttach = () => {
+  const error = chrome.runtime.lastError;
+  if (error) {
+    console.warn(`OnAttach Error:\n ${error.message} `);
   }
-}
+};
 
-// function finishWatchingRequests() {
-//   enabled = false;
-//   console.log(`detach from tab: ${debugTabId}`);
-//   chrome.debugger.detach({ tabId: debugTabId });
-// }
-
-function escapeRegExp(string) {
+const escapeRegExp = (string) => {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-}
+};
 
 // debuggeeId - The debuggee that generated this event.
 // message - Method name. Should be one of the notifications defined by the remote debugging protocol.
 // params - JSON object with the parameters.
-function onEvent(debuggeeId, message, params) {
+const onEvent = (debuggeeId, message, params) => {
   // working only witn our tab
   if (debugTabId !== debuggeeId.tabId) return;
-  if (message === 'Network.requestWillBeSent') {
+  if (
+    message === 'Network.requestWillBeSent' &&
+    !params.request.url.match(/data:image|\.jpeg|\.css|\.gif|\.png|\.ico|\.woff/)
+  ) {
     // useful params:
     // params.requestId ; params.request.url ; (params.requestId, params.redirectResponse)? ;  params.request.method;
     requests[params.requestId] = params.request.url;
-    // remember request id
-    if (params.request.url === '<PATTERN>') {
-      requests[params.requestId] = params.request.url;
-    }
   } else if (message === 'Network.loadingFinished') {
-    // get response body if it is needed request id
+    // get response body if it is request id which we need
     if (!requests.hasOwnProperty(params.requestId)) {
       return;
     }
@@ -56,73 +49,74 @@ function onEvent(debuggeeId, message, params) {
         requestId: params.requestId,
       },
       (response) => {
-        // if response was failed to load by browser then response variable will be undefined and runtime.lastError will appear:
-        // {"code":-32000,"message":"No resource with given identifier found"}
+        const error = chrome.runtime.lastError;
+        if (error) {
+          // if response was failed to load by browser then response variable will be undefined and runtime.lastError will appear:
+          // {"code":-32000,"message":"No resource with given identifier found"}
+          console.warn(`Runtime error from ${requests[params.requestId]}:\n ${error.message}\n  `);
+        }
         goals.forEach((goal) => {
-          if (
-            response &&
-            !requests[params.requestId].match(/data:image|\.jpeg|\.css|\.gif|\.png|\.ico|\.woff/) &&
-            response.body.includes(goal)
-          ) {
-            console.log('FOUND!!! RESPONSE:');
+          if (response && response.body.includes(goal)) {
             const escapedGoal = escapeRegExp(goal);
             const reg = new RegExp(`.{0,40}${escapedGoal}.{0,40}`, 'g');
+
+            // logging in background script console
+            console.log('FOUND!!! RESPONSE:');
             response.body.match(reg).forEach((text) => {
               console.log(text.replace(goal, `%c${goal}%c`), 'color: green', 'color: graydark');
             });
             console.log(`FROM: ${requests[params.requestId]}`);
+            // end of logging
+
             matches.responses[`${goal}`][`${requests[params.requestId]}`] = [];
             regexpMatches = response.body.match(reg);
             regexpMatches.forEach((text) => {
               matches.responses[`${goal}`][`${requests[params.requestId]}`].push(text);
             });
             console.log(`SEND INFO TO INFOTAB: ${infotab.id}`);
+            // TODO: every time send only new match instead of all
             chrome.tabs.sendMessage(infotab.id, { responses: matches.responses });
           }
         });
       }
     );
   }
-}
+};
 
-function attachToTab() {
+const attachToTab = () => {
   chrome.debugger.attach({ tabId: debugTabId }, '1.0', onAttach.bind(null, debugTabId));
   chrome.debugger.onDetach.addListener(() => {
     debugTabId = null;
   });
-}
-function listenMaintabNetwork() {
+};
+const listenMaintabNetwork = () => {
   // Enables network tracking, network events will now be delivered to the client.
   chrome.debugger.sendCommand({ tabId: debugTabId }, 'Network.enable');
   // Fired whenever debugging target issues instrumentation event.
   chrome.debugger.onEvent.addListener(onEvent);
-}
+};
 
-function reloadMainTab() {
+const reloadMainTab = () => {
   // reload tab for watching requests from target page
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.reload(maintabId);
+  chrome.tabs.query({ active: true, currentWindow: true }, () => {
+    chrome.tabs.reload(debugTabId);
   });
-}
+};
 
-function createInfoTab() {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    maintabId = tabs[0].id;
-    chrome.tabs.create(
-      {
-        url: chrome.extension.getURL('infotab.html'),
-      },
-      (tab) => {
-        infotab = tab;
-        console.log(`Create infotab: ${tab.id}`);
-        console.log(`Maintab: ${maintabId}`);
-        reloadMainTab();
-      }
-    );
-  });
-}
+const createInfoTab = () => {
+  chrome.tabs.create(
+    {
+      url: chrome.extension.getURL('infotab.html'),
+    },
+    (tab) => {
+      infotab = tab;
+      reloadMainTab();
+      listenMaintabNetwork();
+    }
+  );
+};
 
-function startWatchingRequests(popupRequest, trackedGoals) {
+const startWatchingRequests = (popupRequest, trackedGoals) => {
   console.log(`GET MESSAGE FROM CONTENT SCRIPT => GOAL:${trackedGoals}`);
   goals = trackedGoals;
   enabled = true;
@@ -138,12 +132,11 @@ function startWatchingRequests(popupRequest, trackedGoals) {
     attachToTab();
   }
   createInfoTab();
-  listenMaintabNetwork();
-}
+};
 
 startFromWhiteList();
 
-// Track if extension icon is clicked
+// Listen if extension icon was clicked
 chrome.browserAction.onClicked.addListener(() => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, 'toggle');
@@ -151,12 +144,13 @@ chrome.browserAction.onClicked.addListener(() => {
 });
 
 // Listening messages from pages
-chrome.runtime.onMessage.addListener((request, sender) => {
+chrome.runtime.onMessage.addListener((request) => {
   if (request.message === 'wanted') {
     startWatchingRequests(request, request.goal);
   } else if (request.message === 'detach') {
+    // use addListener((request, sender) in this case
     // debugTabId = sender.debugTabId;
-    // finishWatchingRequests();
+    // chrome.debugger.detach({ tabId: debugTabId });
   }
 });
 
--- src/content.js
+++ src/content.js
@@ -2,34 +2,38 @@
 console.log('Content script has been loaded succesfully!');
 
 const iframe = document.createElement('iframe');
-iframe.style.background = 'green';
-iframe.style.height = '100%';
-iframe.style.width = '0px';
-iframe.style.position = 'fixed';
-iframe.style.top = '0px';
-iframe.style.right = '0px';
-iframe.style.zIndex = '9000000000000000000';
-iframe.frameBorder = 'none';
-iframe.src = chrome.extension.getURL('index.html');
 
-function toggle() {
+const toggle = () => {
   if (iframe.style.width === '0px') {
     iframe.style.width = '400px';
   } else {
     iframe.style.width = '0px';
   }
-}
+};
+
+const styleIframe = () => {
+  iframe.style.background = 'green';
+  iframe.style.height = '100%';
+  iframe.style.width = '0px';
+  iframe.style.position = 'fixed';
+  iframe.style.top = '0px';
+  iframe.style.right = '0px';
+  iframe.style.zIndex = '9000000000000000000';
+  iframe.frameBorder = 'none';
+  iframe.src = chrome.extension.getURL('index.html');
+};
 
 document.body.appendChild(iframe);
+styleIframe();
 
+// Listen if extension icon was clicked
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg === 'toggle') {
     toggle();
-  } else {
-    console.log(msg);
   }
 });
 
+// send all selected text from a page to React app
 document.onselectionchange = () => {
   const content = window.getSelection().toString().trim();
   if (content === '') return;
